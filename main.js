bff6882993e9ed2f35bd3997bcbcb331cedb7dcd
phr <philippe.reynaud@gmail.com>
2024-01-22 11:25:42 +0100

    wip

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // main.js
 
-// Modules pour la gestion de l'appli et la création de la BrowserWindow native browser window
+//to test desktop version:
+//npm run make
+
+//to make ab exe file:
+//npm run make 
+
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
@@ -10,30 +15,18 @@ const createWindow = () => {
     width: 800,
     height: 600
   })
-
-  // et chargement de l'index.html de l'application.
   mainWindow.loadFile('sources/desktop.html')
-
-  // Ouvrir les outils de développement.
-  // mainWindow.webContents.openDevTools()
 }
 
-// Cette méthode sera appelée quand Electron aura fini
-// de s'initialiser et sera prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-    // Sur macOS il est commun de re-créer une fenêtre  lors 
-    // du click sur l'icone du dock et qu'il n'y a pas d'autre fenêtre ouverte.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quitter quand toutes les fenêtres sont fermées, sauf sur macOS. Dans ce cas il est courant
-// que les applications et barre de menu restents actives jusqu'à ce que l'utilisateur quitte 
-// de manière explicite par Cmd + Q.
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
